0fa2d53107a7610a0b97328b317e13b8cc74a4fb
BrendaLorencon <brenda.silva.avatar@gmail.com>
2021-06-26 00:43:52 -0300

    Adicionado container inicial do app

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        width: 500,
+        height: 500,
+        color: Colors.purple,
+        child: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    ),
+  ));
+}
