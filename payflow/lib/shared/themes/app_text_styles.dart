426758f5b166afe5f99c9d5877d8c9a5d2949fe7
Brenda Lorençon <brenda.silva.avatar@gmail.com>
2021-07-02 11:19:34 -0300

    Adicionado pacote do Google Fonts

---
+++ payflow/lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static final titleHome = GoogleFonts.lexendDeca(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AppColors.heading,
+  );
+}
