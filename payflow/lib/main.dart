48e68f55d74f49b8ab6d7731a2652a1f40712cf9
Brenda Lorençon <brenda.silva.avatar@gmail.com>
2021-07-12 14:02:18 -0300

    Atualizado os plugins para o Firebase

--- payflow/lib/main.dart
+++ payflow/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:payflow/app_widget.dart';
 
 void main() {
   runApp(AppFireBase());
@@ -16,16 +18,26 @@ class _AppFireBaseState extends State<AppFireBase> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return SomethingWentWrong();
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MyAwesomeApp();
-        }
-        return Loading();
-      },
-    );
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Material(
+              child: Center(
+                child: Text(
+                  "Não foi possível inicializar o FireBase",
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
