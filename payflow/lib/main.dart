c02d8628a33ee3185b7610089f5e0e486dee6133
BrendaLorencon <brenda.silva.avatar@gmail.com>
2021-07-11 23:04:32 -0300

    Alterações em plugin

--- payflow/lib/main.dart
+++ payflow/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 import 'modules/login/login_page.dart';
